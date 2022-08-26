f6d466d7b69b88a17489e04fc342daee0bbb19e3
dockrexter <mshaheer.rahi@gmail.com>
2022-08-26 16:52:27 +0500

    auth fixes

--- middleware/firebase-auth.js
+++ middleware/firebase-auth.js
@@ -6,8 +6,16 @@ async function firebaseAuth(req, res, next) {
     try {
         const idToken = req.headers['authorization'].match(regex)?.[1];
         req.token = await getAuth().verifyIdToken(idToken);
-        console.log(req.token, "apple");
-        next();
+        if (req.token.role == "admin") {
+            next();
+        }
+        else {
+            res
+                .status(401)
+                .send({ error: { code: 'unauthenticated' } });
+        }
+        // console.log(req.token, "apple");
+        // next();
     } catch (err) {
         res
             .status(401)
